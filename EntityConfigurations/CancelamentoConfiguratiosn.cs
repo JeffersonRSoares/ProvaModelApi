b2cb1a4dae48d206c0414567b75ad0889ca57dd8
JeffersonRSoares <jefferson.silva15@aluno.senai.br>
2022-08-23 10:35:26 -0300

    atualizado pra entregar valendo nota

--- EntityConfigurations/CancelamentoConfiguratiosn.cs
+++ EntityConfigurations/CancelamentoConfiguratiosn.cs
@@ -7,7 +7,7 @@ public class CancelamentoConfiguration : IEntityTypeConfiguration<Cancelamento>
     public void Configure(EntityTypeBuilder<Cancelamento> builder)
     {
         builder.ToTable("Cancelamentos");
-        builder.HasKey(c => c.Id)
+        builder.HasKey(c => c.Id);
         builder.Property(c => c.Motivo)
                .IsRequired()
                .HasMaxLength(100);
